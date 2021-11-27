e6c3763173bfee550c38c985db6859c33143c8a7
Zhou Pengyu <zhoupengyu@technomous.com.cn>
2021-11-27 08:28:39 +0800

    Add home-manager
    Signed-off-by: Zhou Pengyu <zhoupengyu@technomous.com.cn>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,28 @@
 {
+  description = "Home Manager configurations";
+
   inputs = {
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "flake:nixpkgs";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, homeManager }: {
+    homeConfigurations = {
+      "someuser@somecomputer" = homeManager.lib.homeManagerConfiguration {
+        configuration = {pkgs, ...}: {
+          programs.home-manager.enable = true;
+          home.packages = [ pkgs.hello ];
+        };
+
+        system = "x86_64-linux";
+        homeDirectory = "/home/someuser";
+        username = "someuser";
+        stateVersion = "21.05";
+      };
+    };
   };
 }
 
